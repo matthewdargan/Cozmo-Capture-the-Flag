73e57a159553c6c5149c1a4a5c4ab3de7cbd6eff
Matthew Dargan <matthewdargan57@gmail.com>
2019-02-28 11:46:50 -0600

    Setup basic skeleton for the project

---
+++ capture_the_flag.py
@@ -0,0 +1,19 @@
+"""Capture the Flag game mode for cozmo
+Authors: Matthew Dargan, Daniel Stutz
+"""
+
+import cozmo
+from cozmo.util import degrees
+from cozmo_interface import *
+from colors import Colors
+
+async def cozmo_program(robot: cozmo.robot.Robot):
+    """
+    TODO: comment main function
+    """
+    
+    pass
+
+if __name__ == '__main__':
+    cozmo.run_program(cozmo_program, use_viewer=False, force_viewer_on_top=False)
+
