4a605d4c0ac4f65fddf660a8196202fb40f9776d
Matthew Dargan <matthewdargan57@gmail.com>
2019-03-26 11:44:26 -0500

    Check which operating system the user in on to execute the corresponding xbox controller script

--- capture_the_flag.py
+++ capture_the_flag.py
@@ -4,8 +4,10 @@ Authors: Matthew Dargan, Daniel Stutz
 
 import cozmo
 from cozmo.util import degrees
+import platform
 
-from cozmo_interface import *
+import cozmo_interface
+import xbox_controller
 from colors import Colors
 
 async def cozmo_program(robot: cozmo.robot.Robot):
@@ -16,5 +18,11 @@ async def cozmo_program(robot: cozmo.robot.Robot):
     pass
 
 if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        # run xbox_controller script asynchronously
+
+    else:
+        # run cozmo_interface asynchronously
+
     cozmo.run_program(cozmo_program, use_viewer=False, force_viewer_on_top=False)
 
