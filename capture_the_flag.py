319c697c2d5e3b39e6aeca4071a3b9ef5d5408e2
Daniel Stutz <dstutz@uiowa.edu>
2019-03-28 11:44:09 -0500

    Add Xbox Controller Subprocesses

--- capture_the_flag.py
+++ capture_the_flag.py
@@ -5,6 +5,8 @@ Authors: Matthew Dargan, Daniel Stutz
 import cozmo
 from cozmo.util import degrees
 import platform
+import subprocess
+import sys
 
 import cozmo_interface
 import xbox_controller
@@ -20,9 +22,10 @@ async def cozmo_program(robot: cozmo.robot.Robot):
 if __name__ == '__main__':
     if platform.system() == 'Windows':
         # run xbox_controller script asynchronously
-
+        subprocess.Popen([sys.executable, xbox_controller.cozmo_program], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     else:
         # run cozmo_interface asynchronously
+        subprocess.Popen([sys.executable, cozmo_interface.cozmo_program], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)  
 
     cozmo.run_program(cozmo_program, use_viewer=False, force_viewer_on_top=False)
 
