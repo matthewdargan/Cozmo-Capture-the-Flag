49bed5b570c45889e02428657b52ca4628f7850c
Daniel Stutz <36642158+DanStutz@users.noreply.github.com>
2019-05-03 12:19:17 -0500

    Moved connection to before static robot_origin
    For some reason this works

--- capture_the_flag_master.py
+++ capture_the_flag_master.py
@@ -63,22 +63,25 @@ def cozmo_program(robot: cozmo.robot.Robot, cube_color: cozmo.lights.Light = coz
         else:
             break
 
-    robot_origin: Pose = robot.pose.define_pose_relative_this(Pose(x=robot_distance,
-                                                                   y=0,
-                                                                   z=0,
-                                                                   angle_z=Angle(degrees=0)))
-    robot_origin: Tuple[float, float] = (robot_origin.position.x, robot_origin.position.y)
-
     # send the start message to the network and the number of cubes the slave computers should use
     connection: socket.socket = start_connection("10.0.1.10", 5000)
     connection.send(b'%d' % num_cubes)
 
+    robot_origin: Pose = robot.pose.define_pose_relative_this(Pose(x=robot_distance,
+                                                                   y=0,
+                                                                   z=0,
+                                                                   angle_z=Angle(degrees=0)))
+
+    robot_origin: Tuple[float, float] = (robot_origin.position.x, robot_origin.position.y)
+    
     # setup the game
     robot_cubes, _ = setup(robot, num_cubes, cube_color)
+
 
     # get robot 2's origin
     origin_message: List[str] = receive_message(connection)
     robot2_origin: Tuple[float, float] = (float(origin_message[0]), float(origin_message[1]))
+
 
     # set default scores for each side
     robot1_score: int = 0
